75b7081aa5a912051f2842c1753464c3b845a7ee
Martin <martin@jitbase.com>
2022-06-10 11:22:32 -0400

    working get timeslot to file

--- README.md
+++ README.md
@@ -58,6 +58,12 @@ curl 'https://eservice-api-production.azurewebsites.net/api/GetAvailableTimeslot
 curl 'https://eservice-api-production.azurewebsites.net/api/GetAvailableTimeslots' -X POST -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Accept-Language: fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3' -H 'Accept-Encoding: gzip, deflate, br' -H 'Content-Type: application/json; charset=utf-8' -H 'CrmOrigin: eservices' -H 'Origin: https://eservices.canada.ca' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Referer: https://eservices.canada.ca/' -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' -H 'Sec-Fetch-Site: cross-site' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' --data-raw '{"CrmOrigin":"eservices","BookingId":"1db56752-ffe7-ec11-b5cf-0003ff9c13b7","OfficeIds":["310a32f7-bcac-ea11-a812-000d3af4316a"],"AdditionalParameters":{"UserLocalTime":"2022-06-09T14:33:10.158Z","BookingServiceOfferingIds":["57af4796-67ab-ea11-a812-000d3af4f03d"]}}'
 ```
 
+### Pointe claire
+
+```sh
+curl 'https://eservice-api-production.azurewebsites.net/api/GetAvailableTimeslots' -X POST -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0' -H 'Content-Type: application/json; charset=utf-8' -H 'CrmOrigin: eservices' -H 'Origin: https://eservices.canada.ca' -H 'Referer: https://eservices.canada.ca/' --data-raw '{"CrmOrigin":"eservices","BookingId":"b04f6a5b-4de8-ec11-b5cf-28187805c2cc","OfficeIds":["2d0a32f7-bcac-ea11-a812-000d3af4316a"],"AdditionalParameters":{"UserLocalTime":"2022-06-09T14:23:54.057Z","BookingServiceOfferingIds":["57af4796-67ab-ea11-a812-000d3af4f03d"]}}' | jq -r | jq
+```
+
 ## Output
 
 ```sh
--- scraping.js
+++ scraping.js
@@ -1,30 +1,33 @@
 const axios = require("axios");
-const { wrapper } = require("axios-cookiejar-support");
-const { CookieJar } = require("tough-cookie");
 const cheerio = require("cheerio");
 const fs = require("fs");
 
-axios.defaults.withCredentials = true;
+const client = axios.create({
+  headers: {
+    "User-Agent":
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0",
+  },
+});
 
-const jar = new CookieJar();
-const client = wrapper(axios.create({ jar }));
 const eServiceUrl =
-  "https://eservices.canada.ca/fr/reservation/application/?booking-privacy=true";
+  "https://eservices.canada.ca/en/reservation/application/?booking-privacy=true";
+const eServiceTimeSlotsUrl =
+  "https://eservice-api-production.azurewebsites.net/api/GetAvailableTimeslots";
+
+function getHtmlOptionsFromSelect(root, selector) {
+  const result = {};
+  root(selector)
+    .children("option")
+    .each((id, element) => {
+      const option = root(element);
+      return (result[option.text().toLowerCase()] = option.attr("value"));
+    });
+
+  return result;
+}
 
 async function main() {
-  const axiosOptions = {
-    headers: {
-      "User-Agent":
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0",
-    },
-  };
-
-  await client.get(
-    "https://eservices.canada.ca/en/assets/libs/eServiceFormHelpers.js",
-    axiosOptions
-  );
-
-  const result = await client.get(eServiceUrl, axiosOptions);
+  const result = await client.get(eServiceUrl);
   fs.writeFile("scraping-page1.html", result.data, (err) => {
     if (err) {
       console.error(err);
@@ -35,28 +38,38 @@ async function main() {
 
   const $ = cheerio.load(result.data);
   const bookingData = new URLSearchParams($("#liquid_form").serialize());
-
   const sessionExpiryOn = new Date(new Date().getTime() + 30 * 60 * 1000); // 30 min to ms
+
+  const languageOptions = getHtmlOptionsFromSelect(
+    $,
+    "#esdc_languageofservice"
+  );
+
+  const bookingTypeOptions = getHtmlOptionsFromSelect($, "#esdc_bookingtype");
+  const earliestTravelDateRangeOptions = getHtmlOptionsFromSelect(
+    $,
+    "#esdc_earliesttraveldaterange"
+  );
 
   const params = {
     EntityName: "esdc_booking",
-    languageofservice: "564190001", // FR
-    bookingtype: "564190001", // Passport
-    earliesttraveldaterange: "564190003",
+    languageofservice: languageOptions.french, // FR
+    bookingtype: bookingTypeOptions["passport service"], // Passport
+    earliesttraveldaterange:
+      earliestTravelDateRangeOptions["travel between 6 and 45 business days"], // 9 to 45 jrs
     numberofapplicationspassport: "1",
     passportraapplicable: "1",
     numberofapplicantsbio: "0",
-    sessionexpiryon: "2022-06-09T23:52:13.0000000Z", //encodeURIComponent(sessionExpiryOn.toISOString()),
+    sessionexpiryon: sessionExpiryOn
+      .toISOString()
+      .replace(/\.[0-9]+Z/, ".0000000Z"),
     portalbooking: "1",
     privacystatementaccepted: "1",
     browseragent:
       "Browser+CodeName%3A+Mozilla%0D%0ABrowser+Name%3A+Netscape%0D%0ABrowser+Version%3A+5.0+%28Macintosh%29%0D%0ACookies+Enabled%3A+true%0D%0ABrowser+Language%3A+fr%0D%0ABrowser+Online%3A+true%0D%0APlatform%3A+MacIntel%0D%0AUser-agent+header%3A+Mozilla%2F5.0+%28Macintosh%3B+Intel+Mac+OS+X+10.15%3B+rv%3A101.0%29+Gecko%2F20100101+Firefox%2F101.0",
   };
 
-  bookingData.set(
-    "__EVENTTARGET",
-    "ctl00$ContentContainer$WebFormControl_de5496ccf543ec118c62000d3a09fbc2$NextButton"
-  );
+  bookingData.set("__EVENTTARGET", $("#NextButton").attr("name"));
   bookingData.forEach((value, key) => {
     for (const [keySearch, replacementValue] of Object.entries(params)) {
       if (key.endsWith(keySearch)) {
@@ -65,14 +78,11 @@ async function main() {
     }
   });
 
-  const resultBooking = await client.post(
-    eServiceUrl,
-    bookingData,
-    axiosOptions
-  );
+  const resultBooking = await client.post(eServiceUrl, bookingData);
 
   const pageBooking = cheerio.load(resultBooking.data);
-  console.log(pageBooking("#EntityFormView_EntityID").val());
+  const bookingId = pageBooking("#EntityFormView_EntityID").val();
+  console.log(bookingId);
   fs.writeFile("scraping-page2.html", resultBooking.data, (err) => {
     if (err) {
       console.error(err);
@@ -80,6 +90,34 @@ async function main() {
     }
     console.log("done2");
   });
+
+  const data = {
+    CrmOrigin: "eservices",
+    BookingId: bookingId,
+    OfficeIds: [
+      //"2d0a32f7-bcac-ea11-a812-000d3af4316a", // Pointe claire
+      // "310a32f7-bcac-ea11-a812-000d3af4316a", // Laval
+      // "290a32f7-bcac-ea11-a812-000d3af4316a", // Saint laurent
+      "2f0a32f7-bcac-ea11-a812-000d3af4316a", // Montreal
+    ],
+    AdditionalParameters: {
+      UserLocalTime: new Date().toISOString(),
+      BookingServiceOfferingIds: ["57af4796-67ab-ea11-a812-000d3af4f03d"],
+    },
+  };
+
+  const resultTimeSlot = await client.post(eServiceTimeSlotsUrl, data);
+  fs.writeFile(
+    "result.json",
+    JSON.stringify(JSON.parse(resultTimeSlot.data)),
+    (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log("done3");
+    }
+  );
 }
 
 main();
